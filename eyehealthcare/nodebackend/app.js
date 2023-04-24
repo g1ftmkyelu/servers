2b5efce891d514e826c1f0214ce4419d971c83bc
g1ftmkyelu <mkyelugift@gmail.com>
2023-04-24 04:32:44 +0200

    update

--- eyehealthcare/nodebackend/app.js
+++ eyehealthcare/nodebackend/app.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = new express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
