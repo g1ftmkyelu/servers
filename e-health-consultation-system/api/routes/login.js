b03e16846ae5c59982f152d556a797694b26021f
Gift Mkyelu <62421300+g1ftmkyelu@users.noreply.github.com>
2023-07-11 20:43:25 +0200

    Delete e-health-consultation-system directory

--- e-health-consultation-system/api/routes/login.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const loginController = require('../controllers/login');
-
-router.post('/patient', loginController.patientLogin)
-router.post('/doctor', loginController.doctorLogin)
-
-module.exports = router;
