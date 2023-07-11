bb75d425421e5bc275d8e22eb145ea80bef9e052
Gift Mkyelu <62421300+g1ftmkyelu@users.noreply.github.com>
2023-07-11 20:44:18 +0200

    Delete eyehealthcare directory

--- eyehealthcare/nodebackend/api/routes/login.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const loginController = require('../controllers/login');
-
-router.post('/patient', loginController.patientLogin)
-router.post('/staff', loginController.staffLogin)
-
-module.exports = router;
--- eyehealthcare/nodebackend/api/routes/payment.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller= require('../controllers/payments')
-
-router.post('/pay/stripe', controller.stripe);
-
-module.exports=router
